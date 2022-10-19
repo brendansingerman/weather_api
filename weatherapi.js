697460667c14cbbdf35d579e46cbd2237b0ba160
brendansingerman <brendan@covr.care>
2022-10-18 18:31:12 -0600

    Tested api for weatherapi challenge, waiting on activation for the key

---
+++ weatherapi.js
@@ -0,0 +1,6 @@
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=SanFrancisco&appid={548cadc2a6ef3ed96d4acc5a9cff4482}')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+
+//   api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
